c2adc5a56204932486ddeb7c76e96c8f68e9fa8d
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-07-22 00:35:46 +0530

    Added models/littleboi.js

---
+++ models/littleboi.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const littleboiSchema = new mongoose.Schema({
+  blast_radius: Number,
+  lol: Number,
+  luckyNumber: Number,
+});
+
+module.exports = mongoose.model('littleboi', littleboiSchema);
